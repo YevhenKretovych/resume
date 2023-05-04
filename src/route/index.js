a650f05fab1411a260c9d55377cd975a4f486080
YevhenKretovych <dragon.is@ukr.net>
2023-05-04 20:33:30 +0300

    lesson 2 bootstrap

--- src/route/index.js
+++ src/route/index.js
@@ -1102,6 +1102,16 @@ router.get('/js', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
