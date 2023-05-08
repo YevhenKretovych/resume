38441af9619d8bdb30a62914f7c0bdefed3de781
YevhenKretovych <dragon.is@ukr.net>
2023-05-08 21:38:54 +0300

    lesson 4 bootstrap

--- src/route/index.js
+++ src/route/index.js
@@ -1509,6 +1509,16 @@ router.get('/task31', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/bootstrap2', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap2', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
